7ef8ce282b6a1013ba3c046058caaa32382221ec
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-20 16:13:06 -0400

    may need to fix conditionals

--- data/user/userRouter.js
+++ data/user/userRouter.js
@@ -14,4 +14,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+  const user = await db.getById(req.params.id)
+  if(user) {
+    res.status(200).json(user)
+  } else {
+    res.status(404).json({message: 'user not found for id'})
+  }
+  } catch (error) {
+  console.log(error)
+  res.status(500).json({message: 'Error retreiving the users'})
+  }
+})
+
 module.exports = router;
