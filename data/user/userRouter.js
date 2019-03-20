18bd299d1ce2b84578649489640f87758bc11024
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-03-20 17:20:53 -0400

    MVP Completed
    finished up crud for users and posts. teamed up with group member to
    knock out the custom middleware. good to go

--- data/user/userRouter.js
+++ data/user/userRouter.js
@@ -4,6 +4,7 @@ const db = require("./userDb.js");
 
 const router = express.Router();
 
+
 router.get("/", async (req, res) => {
   try {
     const users = await db.get(req.query);
@@ -16,16 +17,55 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-  const user = await db.getById(req.params.id)
-  if(user) {
-    res.status(200).json(user)
-  } else {
-    res.status(404).json({message: 'user not found for id'})
-  }
+    const user = await db.getById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "user not found for id" });
+    }
   } catch (error) {
-  console.log(error)
-  res.status(500).json({message: 'Error retreiving the users'})
+    console.log(error);
+    res.status(500).json({ message: "Error retreiving the users" });
   }
-})
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await db.insert(req.body);
+    console.log(user);
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await db.remove(req.params.id);
+    if (user > 0) {
+      res.status(200).json({ message: "User has been deleted" });
+    } else {
+      res.status(404).json({ message: "No user to del at target id" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const user = await db.update(req.params.id, req.body);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "No user to update at this id" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,31 @@ const express = require("express");
 
 const helmet = require("helmet");
 
+function forceUpper(req, res, next) {
+  console.log(req.method);
+  //if (req.method === "POST" || req.method === "PUT") {
+  if (req.method === ("POST" || "PUT")) {
+    req.body.name = req.body.name.toUpperCase();
+    console.log(req.body);
+    next();
+  } else {
+    next();
+  }
+}
+
 const postRouter = require("./data/post/postRouter.js");
 const userRouter = require("./data/user/userRouter.js");
+const usersPostsRouter = require("./data/usersPosts/usersPostsRouter.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(forceUpper);
 server.use(helmet());
 
 server.use("/api/posts", postRouter);
 server.use("/api/users", userRouter);
+server.use("/api/usersposts", usersPostsRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h1>Hey There</h1><p>Welcome to the API</p>`);
